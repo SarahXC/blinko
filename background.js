b877add41836c0f77ee465dfcd853bdab6700d63
Sarah Chieng <schieng@mit.edu>
2024-02-24 12:49:42 -0800

    opens new window

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     const urls = request.text.match(/\bhttps?:\/\/\S+/gi);
-    if (urls) {
-        urls.forEach(url => {
-            chrome.tabs.create({url: url});
+    if (urls && urls.length > 0) {
+        // Create a new window
+        chrome.windows.create({url: urls[0], focused: true}, function(window) {
+            // After the window has been created, open the rest of the URLs in new tabs in this window
+            urls.slice(1).forEach(url => {
+                chrome.tabs.create({windowId: window.id, url: url});
+            });
         });
     }
 });
